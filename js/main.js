ba6512906edc924754a554e555d9f9c8538708ec
alibek81 <alimavlonov86@gmail.com>
2026-02-13 10:11:52 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let animals = 2
+
+switch (animals) {
+    case 1:
+        console.log("mushuk")
+        break;
+    case 2:
+        console.log("it")
+        break;
+    case 3:
+        console.log("quyon")
+        break;
+    case 4:
+        console.log("sigir")
+        break;
+}
+
+let a = 2
+console.log()
